b5188561ca0e21e22d70eb1f245aa59899d7773a
Codecamp <Codecamp@Codecamp-Mac-mini-2.local>
2017-02-20 10:01:35 +0100

    Initial Commit

---
+++ uMessage/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  uMessage
+//
+//  Created by Codecamp on 20.02.17.
+//  Copyright © 2017 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ uMessage/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  uMessage
+//
+//  Created by Codecamp on 20.02.17.
+//  Copyright © 2017 Codecamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
