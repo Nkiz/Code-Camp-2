5d87ab7113f2853ab4c7bb83c5ed2877def333fd
Codecamp <Codecamp@codecamp-mac-mini.fritz.box>
2017-03-25 02:52:38 +0100

    set cursor on text field

--- uMessage/ChatViewController.m
+++ uMessage/ChatViewController.m
@@ -383,6 +383,9 @@
                                  };
     
     [self sendMessage:newMessage withTimestamp:timestamp];
+    
+    // set cursor on text field again
+    [self.chatMsg becomeFirstResponder];
 }
 
 /**
